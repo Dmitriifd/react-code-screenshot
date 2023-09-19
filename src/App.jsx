09c074db63a10334e3118246a3b46ccaaf98368f
Dmitriifd <idimka9@gmail.com>
2023-09-19 15:57:38 +0500

    add packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hell</h1>
+      <Button>Hell</Button>
     </>
   );
 }
