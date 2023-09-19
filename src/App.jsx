4cdf82be929dd5ff21b25130abff961608bbffb1
Dmitriifd <idimka9@gmail.com>
2023-09-19 16:26:30 +0500

    add link for styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react';
 import CodeEditor from './components/CodeEditor';
+import { fonts, themes } from './options';
+import useStore from './store';
 
 function App() {
+  const theme = useStore((state) => state.theme);
+  const padding = useStore((state) => state.padding);
+  const fontStyle = useStore((state) => state.fontStyle);
+  const showBackground = useStore((state) => state.showBackground);
 
   return (
     <main className='dark min-h-screen flex justify-center items-center bg-neutral-950 text-white'>
+      <link
+        rel='stylesheet'
+        href={themes[theme].theme}
+        crossOrigin='anonymous'
+      />
+      <link
+        rel='stylesheet'
+        href={fonts[fontStyle].src}
+        crossOrigin='anonymous'
+      />
+
       <CodeEditor />
     </main>
   );
